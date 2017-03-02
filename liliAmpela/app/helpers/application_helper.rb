527385beda29606834f20793f7db8c483f1eb1db
Dorian <gaillotdorian@gmail.com>
2017-03-02 18:30:50 +0100

    customizing cart on the homepage

--- liliAmpela/app/helpers/application_helper.rb
+++ liliAmpela/app/helpers/application_helper.rb
@@ -1,2 +1,18 @@
 module ApplicationHelper
+
+  def custom_link_to_cart(text = nil)
+    text = text ? h(text) : Spree.t(:cart)
+    css_class = nil
+
+    if simple_current_order.nil? || simple_current_order.item_count.zero?
+      text = "#{text}: (#{Spree.t(:empty)})"
+      #css_class = 'empty'
+    else
+      text = "#{text} (#{simple_current_order.item_count})"
+      #css_class = 'full'
+    end
+
+    link_to text.html_safe, spree.cart_path, class: "cart-info item"
+  end
+
 end
