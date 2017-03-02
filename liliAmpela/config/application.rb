72c8b7754545b53c85a180a7c7fa7f489b59da45
Dorian <gaillotdorian@gmail.com>
2017-03-02 20:29:59 +0100

    implementing nice carousel in the homepage

--- liliAmpela/config/application.rb
+++ liliAmpela/config/application.rb
@@ -22,6 +22,8 @@ module LiliAmpela
     end
 
     config.i18n.default_locale = :fr
+
+    config.assets.paths << Rails.root.join("app", "assets", "fonts")
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
