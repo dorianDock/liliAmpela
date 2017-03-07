25ccdbd72f914e7382a5983f6adddfe746a870f8
Dorian <gaillotdorian@gmail.com>
2017-03-07 19:58:58 +0100

    order timeline updated and displayed better

--- liliAmpela/app/helpers/application_helper.rb
+++ liliAmpela/app/helpers/application_helper.rb
@@ -102,4 +102,55 @@ module ApplicationHelper
     end.join("\n")
   end
 
+  def custom_checkout_states
+    @order.checkout_steps
+  end
+
+  def custom_checkout_progress
+    states = custom_checkout_states
+    items = states.map do |state|
+      text = Spree.t("order_state.#{state}").titleize
+
+      css_classes = []
+      current_index = states.index(@order.state)
+      state_index = states.index(state)
+
+      if state_index < current_index
+        css_classes << 'completed'
+        text = link_to text, checkout_state_path(state)
+      end
+
+      css_classes << 'next' if state_index == current_index + 1
+      css_classes << 'current' if state == @order.state
+      css_classes << 'first' if state_index == 0
+      css_classes << 'last' if state_index == states.length - 1
+      # It'd be nice to have separate classes but combining them with a dash helps out for IE6 which only sees the last class
+      class_state_for_step = ''
+      if state == @order.state
+        class_state_for_step = 'active'
+      end
+
+      css_class_for_icon = ''
+      if state == 'address'
+        css_class_for_icon = 'world icon'
+      end
+      if state == 'delivery'
+        css_class_for_icon = 'truck icon'
+      end
+      if state == 'payment'
+        css_class_for_icon = 'payment icon'
+      end
+      if state == 'confirm'
+        css_class_for_icon = 'info icon'
+      end
+      icon_to_use = content_tag('i','', class: css_class_for_icon)
+      title_to_use = content_tag('div', text, class: 'title')
+      content_to_use = icon_to_use+content_tag('div', title_to_use, class: 'content')
+      #content_tag('div', content_tag('span', text), class: 'step '+css_classes.join('-'))
+      content_tag('div', content_to_use, class: 'step '+class_state_for_step+' '+css_classes.join('-'))
+    end
+
+    content_tag('div', raw(items.join("\n")), class: 'ui steps progress-steps', id: "checkout-step-#{@order.state}")
+  end
+
 end
